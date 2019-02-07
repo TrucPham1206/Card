a24772f1b479ac4fd88af6a61f38c6ecc3eb0277
TrucPham1206 <phthitha@sheridancollege.ca>
2019-02-06 19:10:02 -0500

    Checking Branch

---
+++ ABC/src/abc/ABC.java
@@ -0,0 +1,21 @@
+/*
+ * Truc Pham
+ * Student ID: 991511456
+ * SYST10199 - Web Programming
+ */
+package abc;
+
+/**
+ *
+ * @author phamt
+ */
+public class ABC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    
+}
